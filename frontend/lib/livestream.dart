b3e7336012a7db3514b887cbc0aba0d654d826eb
Ruben Gonzalez <rubengaviles@gmail.com>
2023-03-23 19:19:05 +0100

    added cors support

--- frontend/lib/livestream.dart
+++ frontend/lib/livestream.dart
@@ -76,7 +76,7 @@ class _PlayVideoFromNetworkState extends State<PlayVideoFromNetwork> {
   void initState() {
     controller = PodPlayerController(
       playVideoFrom: PlayVideoFrom.youtube(
-        'https://www.youtube.com/watch?v=vhuSuQMpdzo',
+        'https://www.youtube.com/watch?v=UOPgCVEEn5s&t=6s&ab_channel=IIHFWorlds2023',
       ),
     )..initialise();
     super.initState();
