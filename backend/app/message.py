5b6ab41bd131f31eb8d62668d297492437abcdbc
reynisson <reynissonkristofer@gmail.com>
2023-03-24 00:10:33 +0100

    more fixes

--- backend/app/message.py
+++ backend/app/message.py
@@ -5,7 +5,6 @@ class Message:
     def __init__(self, 
                  message : str,
                  username : str):
-        self.timestamp = time.time()
         self.message : str = message
         self.username : str = username
         self.sentiment = sentiment_vader(message).compound
