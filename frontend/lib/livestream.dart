7c4e7e7061328d6c4066b0fba8a0c5208d97478f
Ruben Gonzalez <rubengaviles@gmail.com>
2023-03-23 15:21:08 +0100

    added video

--- frontend/lib/livestream.dart
+++ frontend/lib/livestream.dart
@@ -1,18 +1,9 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class LiveStream extends StatefulWidget {
-  const LiveStream({super.key});
-
-  @override
-  State<StatefulWidget> createState() {
-    return LiveStreamState();
-  }
-}
-
-class LiveStreamState extends State<LiveStream> {
-  @override
+class LiveStream extends StatelessWidget {
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
     double width = screenSize.width * 0.72;
@@ -56,8 +47,76 @@ class LiveStreamState extends State<LiveStream> {
                         Text(
                           "Live",
                           style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                        Center(
+                          child: SizedBox(
+                              height: height * 0.9,
+                              width: width * 0.9,
+                              child: VideoPlayerScreen()),
                         )
                       ]),
                 ))));
+  }
+}
+
+class VideoPlayerScreen extends StatefulWidget {
+  const VideoPlayerScreen({super.key});
+
+  @override
+  State<VideoPlayerScreen> createState() => _VideoPlayerScreenState();
+}
+
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+  late VideoPlayerController _controller;
+  late Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Create and store the VideoPlayerController. The VideoPlayerController
+    // offers several different constructors to play videos from assets, files,
+    // or the internet.
+    _controller = VideoPlayerController.network(
+      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+    );
+
+    // Initialize the controller and store the Future for later use.
+    _initializeVideoPlayerFuture = _controller.initialize();
+
+    // Use the controller to loop the video.
+    _controller.setLooping(true);
+  }
+
+  @override
+  void dispose() {
+    // Ensure disposing of the VideoPlayerController to free up resources.
+    _controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initializeVideoPlayerFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          // If the VideoPlayerController has finished initialization, use
+          // the data it provides to limit the aspect ratio of the video.
+          return AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            // Use the VideoPlayer widget to display the video.
+            child: VideoPlayer(_controller),
+          );
+        } else {
+          // If the VideoPlayerController is still initializing, show a
+          // loading spinner.
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
   }
 }
